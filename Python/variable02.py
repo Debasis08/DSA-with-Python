f00a225071f1690394865eb56e9131467d542223
Debasis08 <patrodebasis8@gmail.com>
2024-04-05 23:41:37 +0530

    Updated

---
+++ Python/variable02.py
@@ -0,0 +1,7 @@
+def myfunc():
+  global x          #declared global variable for first time and given a value
+  x = "fantastic"
+
+myfunc()
+
+print("Python is " + x)     #hence the value will be printed here i.e fantastic
